c8bc3a5ebe195c115fa063125bd3f19037ec738b
Saulo Pereira <saulo.pereira@gmail.com>
2018-12-09 10:12:06 -0200

    make it better

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-var secure = require('ssl-express-www');
+// var secure = require('ssl-express-www');
 
 var enforce = require('express-sslify');
 
 
 server.use(middlewares)
-server.use(secure);
+// server.use(secure);
 server.use(enforce.HTTPS());
 server.use(router)
 server.listen(process.env.PORT || 3000, function() {
