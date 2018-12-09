82120f4b8aaf920542509590b345a3078df315e3
Saulo Pereira <saulo.pereira@gmail.com>
2018-12-09 10:13:43 -0200

    make it better

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 // var secure = require('ssl-express-www');
 
-var enforce = require('express-sslify');
+//var enforce = require('express-sslify');
 
 
 server.use(middlewares)
 // server.use(secure);
-server.use(enforce.HTTPS());
+//server.use(enforce.HTTPS());
 server.use(router)
 server.listen(process.env.PORT || 3000, function() {
     console.log("listening on 3000");
