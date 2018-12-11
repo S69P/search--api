d1dea658ec26ccc6c4f6771e15554668eb085c92
Saulo Pereira <saulo.pereira@gmail.com>
2018-12-11 16:39:55 -0200

    first commit

--- index.js
+++ index.js
@@ -2,14 +2,10 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-// var secure = require('ssl-express-www');
-
-//var enforce = require('express-sslify');
 
 
 server.use(middlewares)
-// server.use(secure);
-//server.use(enforce.HTTPS());
+
 server.use(router)
 server.listen(process.env.PORT || 8080, function() {
     console.log("listening on 8080");
